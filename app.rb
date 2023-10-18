de206e265ea3df2e181819ef5b888ed84c650d00
Mariana Cardozo <146393028+maricardozo-booth@users.noreply.github.com>
2023-10-18 15:35:17 +0000

    graded

--- app.rb
+++ app.rb
@@ -2,8 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:homepage)
+end
+
+get("/process_roll") do
+  @num_of_dice = params.fetch("dice").to_i
+  @dice_sides = params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_of_dice.times do
+    die = rand(1..@dice_sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:process_roll)
 end
